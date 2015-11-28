38203cd5ef58f2427fcb1eaae623ca359ec30f53
Velmurugan Velayutham <mailtovelmuruga@gmail.com>
2015-11-28 06:37:07 +0000

    heroku changes added

--- app.js
+++ app.js
@@ -65,6 +65,8 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
-
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
 module.exports = app;
